481399ddbeea49ed61463b99c506420a948f9ced
Ирул Мечта <123540213+rlzyy@users.noreply.github.com>
2024-11-08 08:00:29 +0700

    Update script.js

--- plugins/script.js
+++ plugins/script.js
@@ -3,7 +3,7 @@ const helper = {
   category: 'info',
   description: 'Original script bot',
   operate: async (ctx) => {
-    await ctx.reply('Original Script: https://github.com/rlzyy\n(This base is 100% made by *Rlzyy* If you claim this is mine and sell it, be prepared to bear the risk.)\n\n\n[ Thanks To ]\n- Telegraf & Module Creator\n- Rlzyy (Base Maker)\n- Muhammad Adriansyah\n -KyuuRzy\n- Naraa\n- XYZ Teams');
+    await ctx.reply('Original Script: https://github.com/rlzyy/izumii-bot.git\n(This base is 100% made by *Rlzyy* If you claim this is mine and sell it, be prepared to bear the risk.)\n\n\n[ Thanks To ]\n- Telegraf & Module Creator\n- Rlzyy (Base Maker)\n- Muhammad Adriansyah\n -KyuuRzy\n- Naraa\n- XYZ Teams');
   },
   noPrefix: false,
   isOwner: false,
